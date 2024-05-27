8abd710ab53431fd01dd97fbbb861140e6ea8697
Selim Sutcu <bayram.selim57@gmail.com>
2024-05-27 22:03:19 +0200

    3

---
+++ src/components/Firebase/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const config = {
+  apiKey: "AIzaSyAja77YOvpTKqM2293Q-4u_XaWBAccFUAQ",
+  authDomain: "hero-quiz-57f40.firebaseapp.com",
+  projectId: "hero-quiz-57f40",
+  storageBucket: "hero-quiz-57f40.appspot.com",
+  messagingSenderId: "575835359146",
+  appId: "1:575835359146:web:469b3fc02a14a14ff4cdd3",
+  measurementId: "G-3LMBTY5X1P",
+};
+
+const app = initializeApp(config);
