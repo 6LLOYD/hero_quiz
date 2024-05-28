72866e2f262dc8792b6c824f08549f32ed37be48
Selim Sutcu <bayram.selim57@gmail.com>
2024-05-28 16:18:29 +0200

    5

--- src/components/SignUp/index.js
+++ src/components/SignUp/index.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../Firebase/firebaseConfig";
+import { Link, useNavigate } from "react-router-dom";
 
-const SignUp = () => {
+const SignUp = (props) => {
   const data = {
     pseudo: "",
     email: "",
@@ -10,11 +13,72 @@ const SignUp = () => {
 
   const [loginData, setLoginData] = useState(data);
 
+  const [error, setError] = useState("");
+
+  const navigate = useNavigate();
+
+  // Gestion valeur inputs
   const handleChange = (e) => {
     setLoginData({ ...loginData, [e.target.id]: e.target.value });
   };
 
+  // Gestion Form submit
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { email, password } = loginData;
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((user) => {
+        setLoginData({ ...data });
+        navigate("/home");
+      })
+      .catch((error) => {
+        setError(error);
+        setLoginData({ ...data });
+      });
+  };
+
+  // Gestion btn inscription
   const { pseudo, email, password, confirmPassword } = loginData;
+
+  const btn =
+    pseudo === "" ||
+    email === "" ||
+    password === "" ||
+    confirmPassword === "" ||
+    password !== confirmPassword ? (
+      <button disabled>Inscription</button>
+    ) : (
+      <button>Inscription</button>
+    );
+
+  // Gestion erreurs
+  const errorMsg = error !== "" && <span>{error.message}</span>;
+
+  // Gestion deepNotes
+  const styleEm = password.length >= 6 ? "green" : "red";
+
+  const isPasswordMatch =
+    password === confirmPassword &&
+    password !== "" &&
+    confirmPassword !== "" ? (
+      <em
+        style={{
+          fontSize: "10px",
+          color: "green",
+        }}
+      >
+        - Les mots de passe sont identiques -
+      </em>
+    ) : (
+      <em
+        style={{
+          fontSize: "10px",
+          color: "red",
+        }}
+      >
+        - Les mots de passe ne sont pas identiques -
+      </em>
+    );
 
   return (
     <div className="signUpLofinBox">
@@ -22,7 +86,8 @@ const SignUp = () => {
         <div className="formBoxLeftSignup"></div>
         <div className="formBoxRight">
           <div className="formContent">
-            <form>
+            <form onSubmit={handleSubmit}>
+              {errorMsg}
               <h2>Inscription</h2>
               <div className="inputBox">
                 <input
@@ -50,22 +115,29 @@ const SignUp = () => {
                   Email
                 </label>
               </div>
+
               <div className="inputBox">
                 <input
-                  type="text"
+                  type="password"
                   id="password"
                   required
                   autoComplete="off"
                   onChange={handleChange}
                   value={password}
                 />
+
                 <label htmlFor="password" className="">
                   Mot de passe
                 </label>
               </div>
-              <div className="inputBox">
+              <div
+                className="inputBox"
+                style={{
+                  marginBottom: "70px",
+                }}
+              >
                 <input
-                  type="text"
+                  type="password"
                   id="confirmPassword"
                   required
                   autoComplete="off"
@@ -75,8 +147,27 @@ const SignUp = () => {
                 <label htmlFor="confirmPassword" className="">
                   Confirmer le mot de passe
                 </label>
+                {password.length > 1 && (
+                  <em
+                    style={{
+                      fontSize: "10px",
+                      color: styleEm,
+                    }}
+                  >
+                    - le mot de passe doit contenir au moins 6 caractères -
+                  </em>
+                )}
+                <br />
+                {confirmPassword.length > 1 && isPasswordMatch}
               </div>
+
+              {btn}
             </form>
+            <div className="linkContainer">
+              <Link to="/login" className="simpleLink">
+                Déjà inscrit? Connectez-vous.
+              </Link>
+            </div>
           </div>
         </div>
       </div>
