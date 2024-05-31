86c9c0f3432611586ca1c6d26966145cd43f4528
Selim Sutcu <bayram.selim57@gmail.com>
2024-05-31 23:20:43 +0200

    Récupérer les questions à poser dans le quiz

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
 import Levels from "../Levels";
 import ProgressBar from "../ProgressBar";
+import { QuizMarvel } from "../quizMarvel";
 
 class Quiz extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      levelsNames: ["debutant", "confirme", "expert"],
+      quizLevel: 0,
+      maxQuestions: 10,
+      storedQuestions: [],
+      question: null,
+      options: [],
+    };
+  }
+
+  loadQuestions = (level) => {
+    const fetchedArrayQuiz = QuizMarvel[0].quizz[level];
+    if (fetchedArrayQuiz >= this.state.maxQuestions) {
+      const newArray = fetchedArrayQuiz.map((answer, ...keepRest) => keepRest);
+      this.setState({ storedQuestions: newArray });
+    } else {
+      console.log("pas assez de questions!");
+    }
+  };
+  componentDidMount() {
+    this.loadQuestions(this.state.levelsNames[this.state.quizLevel]);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions.length !== prevState.storedQuestions) {
+      this.setState({ question: this.state.storedQuestions });
+    }
   }
   render() {
     const { pseudo } = this.props.userData;
