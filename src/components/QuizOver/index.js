8b561ff61cfea8fd9ba6dd07ad958af36102dde1
Selim Sutcu <bayram.selim57@gmail.com>
2024-06-05 15:03:07 +0200

    LocalStorage pour stocker la data de Marvel API

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -17,21 +17,47 @@ const QuizOver = React.forwardRef((props, ref) => {
 
   useEffect(() => {
     setAsked(ref.current);
+
+    if (localStorage.getItem("marvelStorageDate")) {
+      const date = localStorage.getItem("marvelStorageDate");
+      checkDataAge(date);
+    }
   }, [ref]);
+
+  const checkDataAge = (date) => {
+    var today = Date.now();
+    const timeDifference = today - date;
+
+    const daysDifference = timeDifference / (1000 * 3600 * 24);
+
+    if (daysDifference >= 15) {
+      localStorage.clear();
+      localStorage.setItem("marvelStorageDate", Date.now());
+    }
+  };
 
   const showModal = (id) => {
     setOpenModal(true);
-    axios
-      .get(
-        `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=${API_PUBLIC_KEY}&hash=${hash}`
-      )
-      .then((response) => {
-        setCharactersInfos(response.data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    if (localStorage.getItem(id)) {
+      setCharactersInfos(JSON.parse(localStorage.getItem(id)));
+      setLoading(false);
+    } else {
+      axios
+        .get(
+          `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=${API_PUBLIC_KEY}&hash=${hash}`
+        )
+        .then((response) => {
+          setCharactersInfos(response.data);
+          setLoading(false);
+          localStorage.setItem(id, JSON.stringify(response.data));
+          if (!localStorage.getItem("marvelStorageDate")) {
+            localStorage.setItem("marvelStorageDate", Date.now());
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
   };
 
   const hideModal = () => {
