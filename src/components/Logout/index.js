8eb0562e12eed21ee1e05b2b76ef4a889eb27b68
Selim Sutcu <bayram.selim57@gmail.com>
2024-06-05 13:05:55 +0200

    Afficher une info-bulle via react-tooltip

--- src/components/Logout/index.js
+++ src/components/Logout/index.js
@@ -3,24 +3,23 @@ import { signOut } from "firebase/auth";
 import { auth } from "../Firebase/firebaseConfig";
 import { useNavigate } from "react-router-dom";
 import logout from "./logout.png";
+import { Tooltip } from "react-tooltip";
 
 const Logout = () => {
   const [checked, setChecked] = useState(false);
-
   const navigate = useNavigate();
 
   useEffect(() => {
     if (checked) {
       signOut(auth)
         .then(() => {
-          console.log("dec");
+          console.log("Déconnexion réussie");
           setTimeout(() => {
             navigate("/", { replace: true });
           }, 1000);
         })
         .catch((error) => {
-          // An error happened.
-          console.error("Sign out error", error);
+          console.error("Erreur de déconnexion", error);
         });
     }
   }, [checked, navigate]);
@@ -31,18 +30,21 @@ const Logout = () => {
 
   return (
     <div className="logoutContainer">
-      <img
-        src={logout}
-        alt="Logout"
-        onClick={handleLogout}
-        style={{
-          cursor: "pointer",
-          width: "30px", // Taille fixe de l'image
-          height: "auto",
-          maxWidth: "100%", // Empêche l'image de dépasser la taille de son conteneur
-          maxHeight: "100px", // Limite la hauteur maximale
-        }}
-      />
+      <span data-tooltip-id="logoutTooltip" data-tooltip-content="Déconnexion">
+        <img
+          src={logout}
+          alt="Logout"
+          onClick={handleLogout}
+          style={{
+            cursor: "pointer",
+            width: "30px", // Taille fixe de l'image
+            height: "auto",
+            maxWidth: "100%", // Empêche l'image de dépasser la taille de son conteneur
+            maxHeight: "100px", // Limite la hauteur maximale
+          }}
+        />
+      </span>
+      <Tooltip id="logoutTooltip" place="left" effect="float" />
     </div>
   );
 };
