cbb74c207215d31b4e9bc3ba9bae88446edc68ef
Selim Sutcu <bayram.selim57@gmail.com>
2024-06-05 13:33:22 +0200

    Afficher un modal (popUp) dans React

--- src/components/QuizOver/index.js
+++ src/components/QuizOver/index.js
@@ -1,14 +1,24 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { GiTrophyCup } from "react-icons/gi";
-
+import Modal from "../Modal";
 const QuizOver = React.forwardRef((props, ref) => {
   const { levelNames, score, maxQuestions, quizLevel, loadLevelQuestions } =
     props;
   const [asked, setAsked] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
 
   useEffect(() => {
     setAsked(ref.current);
   }, [ref]);
+
+  const showModal = (id) => {
+    setOpenModal(true);
+  };
+
+  const hideModal = () => {
+    setOpenModal(false);
+  };
+
   const averageGrade = maxQuestions / 2;
 
   if (score < averageGrade) {
@@ -71,7 +81,12 @@ const QuizOver = React.forwardRef((props, ref) => {
             <td>{question.question}</td>
             <td>{question.answer}</td>
             <td>
-              <button className="btnInfo">Infos</button>
+              <button
+                className="btnInfo"
+                onClick={() => showModal(question.heroId)}
+              >
+                Infos
+              </button>
             </td>
           </tr>
         );
@@ -102,6 +117,17 @@ const QuizOver = React.forwardRef((props, ref) => {
           <tbody>{questionAnswer}</tbody>
         </table>
       </div>
+      <Modal showModal={openModal} hideModal={hideModal}>
+        <div className="modalHeader">
+          <h2>Titre</h2>
+        </div>
+        <div className="modalBody">
+          <h3>Titre</h3>
+        </div>
+        <div className="modalFooter">
+          <button className="modalBtn">Fermer</button>
+        </div>
+      </Modal>
     </Fragment>
   );
 });
